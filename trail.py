b838d1713fb207168cb5bd0235902acd110cebf4
aaryaamoharir <66750249+aaryaamoharir@users.noreply.github.com>
2026-03-25 15:33:33 -0500

    added two more vulnerabilities

--- trail.py
+++ trail.py
@@ -5,3 +5,11 @@ def get_user(username):
 import os
 def ping_host(host):
     os.system("ping -c 1 " + host)
+
+import pickle
+
+def load_data(data):
+    return pickle.loads(data)
+
+DB_PASSWORD = "supersecret123"
+
